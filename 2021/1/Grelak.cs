2a4b64cd0ed48e8d3d5af53cfb66c1d5f0976dc3
Juan Legaz <juan.legaz.t@gmail.com>
2021-12-01 16:16:17 +0000

    format newlines

--- 2021/1/Grelak.cs
+++ 2021/1/Grelak.cs
@@ -4,12 +4,9 @@ using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 
-namespace ConsoleApp1
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace ConsoleApp1 {
+    class Program {
+        static void Main(string[] args) {
             var list = File.ReadAllLines("E:/Input.txt");
             List<int> intlist = list.Select(x => int.Parse(x)).ToList();
 
@@ -17,19 +14,13 @@ namespace ConsoleApp1
 
             bool option = Console.Read() == 'a';
 
-            for(int i = option ? 1 : 3; i < intlist.Count; i++)
-            {
-                if (option)
-                {
-                    if (intlist[i] > intlist[i - 1])
-                    {
+            for (int i = option ? 1 : 3; i < intlist.Count; i++) {
+                if (option) {
+                    if (intlist[i] > intlist[i - 1]) {
                         increases++;
                     }
-                }
-                else
-                {
-                    if (intlist[i - 3] + intlist[i - 2] + intlist[i - 1] < intlist[i - 2] + intlist[i - 1] + intlist[i])
-                    {
+                } else {
+                    if (intlist[i - 3] + intlist[i - 2] + intlist[i - 1] < intlist[i - 2] + intlist[i - 1] + intlist[i]) {
                         increases++;
                     }
                 }
