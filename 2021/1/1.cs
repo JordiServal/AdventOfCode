a542ea4538f83e89b312f53e9cbd4c380833427a
Juan Legaz <juan.legaz.t@gmail.com>
2021-12-01 15:52:34 +0000

    First day

---
+++ 2021/1/1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = File.ReadAllLines("E:/Input.txt");
+            List<int> intlist = list.Select(x => int.Parse(x)).ToList();
+
+            int increases = 0;
+
+            bool option = Console.Read() == 'a';
+
+            for(int i = option ? 1 : 3; i < intlist.Count; i++)
+            {
+                if (option)
+                {
+                    if (intlist[i] > intlist[i - 1])
+                    {
+                        increases++;
+                    }
+                }
+                else
+                {
+                    if (intlist[i - 3] + intlist[i - 2] + intlist[i - 1] < intlist[i - 2] + intlist[i - 1] + intlist[i])
+                    {
+                        increases++;
+                    }
+                }
+            }
+
+            Console.WriteLine(increases);
+        }
+    }
+}
