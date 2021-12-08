be8fee73ba5c11b176a7997fc75a7952e4489daa
JordiSerVal <sernavalero96@gmail.com>
2021-12-08 22:11:27 +0100

    Dia 8 jordi

---
+++ 2021/8/JordiServal.js
@@ -0,0 +1,20 @@
+const input = `be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce`
+
+const getDigits = (i) => {
+  return i.reduce((acc, line) => {
+    line = line.split(' | ').pop()
+    line = line.split(' ')
+    return acc + line.reduce((lineAcc, digit) => digit.length === 2 || digit.length === 4 || digit.length === 3 || digit.length === 7 ? lineAcc + 1 : lineAcc, 0)
+  }, 0)
+}
+
+console.log("Singular digits: ", getDigits(input.split('\n')))
