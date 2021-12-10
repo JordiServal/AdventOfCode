8d07eb339065d71d89bffdffa115ab23c0f99349
Jordi_SerVal <sernavalero96@gmail.com>
2021-12-10 10:36:09 +0100

    Part2

--- 2021/10/JordiServal.js
+++ 2021/10/JordiServal.js
@@ -31,4 +31,34 @@ const input = `[({(<(())[]>[[{[]{<()<>>
     }, 0)
   }
   
+  const getFinishedLines = (i) => {
+    let linesScores = i.reduce((autoCompleteLines, line) => {
+      const pairsLeft = line.split('').reduce((currentLine, char) => {
+        if(currentLine !== false) {
+          if(!closing.includes(char)) {
+            currentLine.push(char)
+          } else {
+            const initChar = currentLine.pop()
+            if(initChar === '(' && char !== ')' ||
+              initChar === '[' && char !== ']' ||
+              initChar === '{' && char !== '}' ||
+              initChar === '<' && char !== '>') return false
+          }
+        }
+        return currentLine
+      }, [])
+      if(pairsLeft !== false) {
+        const lineScore = pairsLeft.reverse().reduce((score, char) => {
+          return score * 5 + (char === '(' ? 1 : char === '[' ? 2 : char === '{' ? 3 : char === '<' ? 4 : 0)
+        }, 0)
+        autoCompleteLines.push(lineScore)
+      }
+      return autoCompleteLines 
+    }, [])
+    return linesScores.sort((a, b) => a-b)[Math.floor(linesScores.length / 2)]
+  }
+  
   console.log("Corrupted lines:", getCorruptedIndex(input.split('\n')))
+  console.log("Finish lines:", getFinishedLines(input.split('\n')))
+  
+  
