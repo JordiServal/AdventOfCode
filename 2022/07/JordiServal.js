4d435e02d9056b2adcf455cb907b722cf9c9d20c
JordiSerVal <sernavalero96@gmail.com>
2022-12-13 00:42:15 +0100

    Day 7 unfinished

--- 2022/07/JordiServal.js
+++ 2022/07/JordiServal.js
@@ -6,22 +6,66 @@ let input = fs
   .toString()
   .trim()
 
-const parseInput = (input) => {
-  return input.split("$ ").map((command) => command.split('\n').map((line) => line.trim().split(' ')))
-}  
 
-const createStructure = (commands) => {
-  commands.shift()
-  return commands.reduce((struc, cmd) => {
-    const [[order, args], ] = cmd
-    console.log(cmd, order, args)
-  }, {})
+const parseInput = (inp) => {
+  return inp.split('$ ').filter((c)=>c!=='').map((cmd)=> {
+    return cmd.trim('\n').split('\r\n').map((line)=>line.split(' '))
+  })
 }
+    
 
 const commands = parseInput(input)
 
-const part1 = createStructure(commands)
+const createStructure = () => {
+  let struc = {}
+  do {
+    const cmd = commands.shift()
+    if(cmd.length === 1) {
+      // CD
+      const dir = cmd[0][1]
+      if(dir === '..') {
+        return struc
+      } else {
+        struc[dir] = createStructure()
+      }
+    } else {
+      // LS
+      cmd.shift()
+      cmd.forEach((line) => {
+        if(line[0] === 'dir') 
+          struc[line[1]] = {}
+        else
+          struc[line[1]] = parseInt(line[0])
+      })
+    }
+  } while(commands.length > 0)
+  return struc
+}
+
+const getDirSize = (struct, size = 0, min=0) => {
+  let dirSize = 0
+  Object.keys(struct).forEach((key) => {
+    console.log(key, dirSize, size, struct[key])
+    if(typeof struct[key] === 'object') {
+      const dir = getDirSize(struct[key], size, min)
+      if(dir <= min ) {
+        size += dir
+      }
+      dirSize += dir
+    } else {
+      dirSize += struct[key]
+    }
+  })
+  if(dirSize <= min) {
+    size += dirSize
+  }
+  return size
+}
+
+
+const part1 = getDirSize(createStructure(commands), 0, 100000)
 const part2 = ''
 
 
+// console.log(part1)
 console.log({part1, part2})
