29b464f0dab7c87738f9ef9368f843f0fe9c975a
Jordi Serna Valero <sernavalero96@gmail.com>
2023-12-02 12:43:38 +0000

    p2 2 2023

--- 2023/02/JordiServal.js
+++ 2023/02/JordiServal.js
@@ -12,14 +12,12 @@ const getPossibleGames = (games, condition) => {
   return games.map(([id, rounds]) => {
     const game = {}
     game.id = parseInt(id.split(' ')[1])
-    console.log(game.id)
     game.dices = !rounds.split(';').map(round => {
       return round.split(',').reduce((acc, curr) => {
         const [cont, color] = curr.trim().split(' ')
         return {...acc, [color]: parseInt(cont)}
       }, {})
     }).some(round => {
-      console.log(round)
       if(round.red && round.red > condition.red) return true
       if(round.green && round.green > condition.green) return true
       if(round.blue && round.blue > condition.blue) return true
@@ -31,19 +29,20 @@ const getPossibleGames = (games, condition) => {
 
 const getPowerGames = games => {
   return games.map(([id, rounds]) => {
-    const game = {}
-    game.id = parseInt(id.split(' ')[1])
-    const minimum = { red: Infinity, green: Infinity, blue: Infinity}
+    
+    const minimum = { red: 0, green: 0, blue: 0}
     rounds.split(';').forEach(round => {
       round.split(',').forEach(dice => {
-        const [cont, color] = dice.trim().split(' ')
-        if(color === '' && cont < minimum.red) minimum.red = cont
-        if(color === '' && cont < minimum.green) minimum.green = cont
-        if(color === '' && cont < minimum.blue) minimum.blue = cont
+        let [cont, color] = dice.trim().split(' ')
+        cont = parseInt(cont)
+        if(color === 'red' && cont > minimum.red) minimum.red = cont
+        if(color === 'green' && cont > minimum.green) minimum.green = cont
+        if(color === 'blue' && cont > minimum.blue) minimum.blue = cont
     })
     })
-    return game
-  })
+    
+    return Object.values(minimum).reduce((acc, curr) => acc * curr)
+  }).reduce((acc, curr) => acc + curr)
 }
 
 const condition = {
